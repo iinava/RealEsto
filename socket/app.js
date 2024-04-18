2211941a8d8543ffa48e89383cf8ecf97719cdb8
iinava <131179814+iinava@users.noreply.github.com>
2024-04-19 00:15:38 +0530

    feat(chat): Implement real-time chat using Socket.IO

---
+++ socket/app.js
@@ -0,0 +1,45 @@
+import { Server } from "socket.io";
+
+const io = new Server({
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+let onlineUser = [];
+
+const addUser = (userId, socketId) => {
+  const userExits = onlineUser.find((user) => user.userId === userId);
+  if (!userExits) {
+    onlineUser.push({ userId, socketId });
+  }
+};
+
+const removeUser = (socketId) => {
+  onlineUser = onlineUser.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return onlineUser.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  socket.on("newUser", (userId) => {
+    addUser(userId, socket.id);
+    console.log(onlineUser,"online users");
+  });
+
+  socket.on("sendMessage", ({receiverId, data }) => {
+    console.log(receiverId,data,'njn ingot varunnund');
+    const receiver = getUser(receiverId);
+    console.log(receiver,"receiver");
+
+    io.to(receiver.socketId).emit("getMessage", data);
+  });
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+  });
+});
+
+io.listen("4000");
